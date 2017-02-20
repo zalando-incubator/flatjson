be8b1c39950322acd3919ca36d6efeca8bdd290a
Tim Lossen <tim@lossen.de>
2017-02-20 23:27:28 +0100

    improved sample generator

--- test/generator.rb
+++ test/generator.rb
@@ -1,42 +1,61 @@
 require "json"
 require "random_data"
+require "securerandom"
+
 
 class Random
-  COLORS = %w[aliceblue antiquewhite aqua aquamarine azure beige bisque black 
-    blanchedalmond blue blueviolet brown burlywood cadetblue chartreuse chocolate 
-    coral cornflowerblue cornsilk crimson cyan darkblue darkcyan darkgoldenrod 
-    darkgray darkgreen darkkhaki darkmagenta darkolivegreen darkorange darkorchid 
-    darkred darksalmon darkseagreen darkslateblue darkslategray darkturquoise 
-    darkviolet deeppink deepskyblue dimgray dodgerblue firebrick floralwhite 
-    forestgreen fuchsia gainsboro ghostwhite gold goldenrod gray green greenyellow 
-    honeydew hotpink indianred indigo ivory khaki lavender lavenderblush lawngreen 
-    lemonchiffon lightblue lightcoral lightcyan lightgoldenrodyellow lightgreen 
-    lightgrey lightpink lightsalmon lightseagreen lightskyblue lightslategray 
-    lightsteelblue lightyellow lime limegreen linen magenta maroon mediumaquamarine 
-    mediumblue mediumorchid mediumpurple mediumseagreen mediumslateblue 
-    mediumspringgreen mediumturquoise mediumvioletred midnightblue mintcream 
-    mistyrose moccasin navajowhite navy oldlace olive olivedrab orange orangered 
-    orchid palegoldenrod palegreen paleturquoise palevioletred papayawhip peachpuff 
-    peru pink plum powderblue purple red rosybrown royalblue saddlebrown salmon 
-    sandybrown seagreen seashell sienna silver skyblue slateblue slategray snow 
-    springgreen steelblue tan teal thistle tomato turquoise violet wheat white 
-    whitesmoke yellow yellowgreen]
-  
+  COLORS = %w[
+    aquamarine azure beige black blue brown chartreuse chocolate coral 
+    cornsilk crimson cyan firebrick forestgreen fuchsia gainsboro ghostwhite 
+    gold gray green honeydew hotpink indianred indigo ivory khaki lavender 
+    lawngreen lime linen magenta maroon navy olive orange orchid pink plum 
+    purple red royalblue salmon sandybrown seagreen seashell sienna silver 
+    skyblue slategray snow tan teal thistle tomato turquoise violet wheat 
+    white yellow
+  ]  
   def self.color
     COLORS.sample
   end
 end
 
-def basic(level)
-  [nil, true, false].sample
+
+def bool
+  [true, false, true, false, nil].sample
 end
 
-def array(level)
-  Array.new(rand(2..8)) { value(level - 1) }
+def int
+  rand(-99_999..99_999)
 end
 
-def record(level)
+def float
+  (int * rand).round(rand(3..5))
+end
+
+def array_int
   {
+    data: Array.new(rand(3..12)) { int }
+  }
+end
+
+def array_float
+  {
+    metrics: Array.new(rand(3..12)) { float }
+  }
+end
+
+def array_bool
+  {
+    settings: Array.new(rand(3..12)) { bool }
+  }
+end
+
+def array_data
+  send(%i[array_bool array_int array_float].sample)
+end
+
+def record
+  {
+    record_id: SecureRandom.uuid,
     first_name: Random.firstname,
     last_name: Random.lastname,
     email: Random.email,
@@ -44,36 +63,54 @@ def record(level)
   }
 end
 
-def localizer
-  Random.paragraphs.split.sample(rand(3..6)).join("_")
-end
-
-def localized(level)
+def localized
   {
-    _localized: %w[de en fi nl it es pt].inject({}) { |hash, key| hash[key] = localizer; hash }
+    localized: %w[de en fi nl it es pt].inject({}) do |hash, key| 
+      hash[key] = Random.paragraphs.split.sample(rand(3..6)).join("_")
+      hash 
+    end
   }
 end
 
-def text(level)
-  Random.paragraphs.strip
+def text
+  {
+    abstract: Random.paragraphs.strip
+  }
 end
 
-def value(level)
-  options = %w[basic text record localized]
-  options += %w[array array hash hash] if level > 0
-  send(options.sample, level - 1)
+def value
+  send(%i[array_data record localized text].sample)
 end
 
-def key
-  Random.alphanumeric(rand(5..12))
-end
-
-def hash(level)
-  result = {}
+def hash
+  result = { data_id: SecureRandom.uuid }
   rand(2..8).times do 
-    result[key] = value(level - 1)
+    result[Random.color] = value
   end
   result
 end
 
-puts hash(6).to_json
+def entry
+  result = { entry_id: SecureRandom.uuid }
+  rand(5..7).times do 
+    result[Random.color] = hash
+  end
+  result
+end
+
+def list
+  Array.new(rand(7..12)) { entry }
+end
+
+def message
+  {
+    created_at: Time.now.to_s,
+    message_id: SecureRandom.uuid,
+    offset: rand(9999999),
+    data: list
+  }
+end
+
+
+puts message.to_json
+
