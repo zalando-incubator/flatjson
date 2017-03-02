aa1309ea80ed2a3c32017ea172d2384ee6511da7
Tim Lossen <tim.lossen@zalando.de>
2017-03-02 13:49:54 +0100

    refactoring: use explicit package names

--- src/jmh/java/bench/ParseSample.java
+++ src/jmh/java/bench/ParseSample.java
@@ -1,15 +1,9 @@
 package bench;
 
-import com.eclipsesource.json.JsonValue;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.google.gson.Gson;
-import org.boon.json.JsonFactory;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.zalando.flatjson.Json;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -20,31 +14,34 @@ import java.util.Map;
 public class ParseSample {
 
     private String sample;
+    private com.fasterxml.jackson.databind.ObjectReader jackson;
+    private com.google.gson.Gson gson;
+    private org.boon.json.ObjectMapper boon;
 
     @Setup public void setup() throws IOException {
         sample = new String(Files.readAllBytes(Paths.get("test/colors.json")));
+        jackson = new com.fasterxml.jackson.databind.ObjectMapper().reader(Map.class);
+        gson = new com.google.gson.Gson();
+        boon = org.boon.json.JsonFactory.create();
     }
 
-    @Benchmark public Json flatjson() {
-        return Json.parse(sample);
+    @Benchmark public org.zalando.flatjson.Json flatjson() {
+        return org.zalando.flatjson.Json.parse(sample);
     }
 
-    @Benchmark public JsonValue minimaljson() {
+    @Benchmark public com.eclipsesource.json.JsonValue minimaljson() {
         return com.eclipsesource.json.Json.parse(sample);
     }
 
     @Benchmark public Map jackson() throws IOException {
-        ObjectReader reader = new ObjectMapper().reader(Map.class);
-        return reader.readValue(sample);
+        return jackson.readValue(sample);
     }
 
     @Benchmark public Map gson() {
-        Gson gson = new Gson();
         return gson.fromJson(sample, Map.class);
     }
 
     @Benchmark public Map boon() {
-        org.boon.json.ObjectMapper mapper = JsonFactory.create();
-        return mapper.readValue(sample, Map.class);
+        return boon.readValue(sample, Map.class);
     }
 }
