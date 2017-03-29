7e9761b33f0f591ab36b785a16ae6c55162c4957
Tim Lossen <tim@lossen.de>
2017-03-29 09:23:08 +0200

    test cleanup

--- src/test/java/org/zalando/flatjson/PrintTest.java
+++ src/test/java/org/zalando/flatjson/PrintTest.java
@@ -3,32 +3,26 @@ package org.zalando.flatjson;
 import org.junit.Test;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class PrintTest {
 
     @Test public void printObjectWithEscapedKey() {
-        Json json = Json.parse("{\"\\noo\\b\": true }");
-        assertTrue(json.isObject());
-        Map<String, Json> object = json.asObject();
-        assertEquals("{\"\\noo\\b\":true}", object.toString());
+        Json json = Json.object();
+        json.asObject().put("\noo\b", Json.value(true));
+        assertEquals("{\"\\noo\\b\":true}", json.toString());
     }
 
     @Test public void printReversedArray() {
         Json json = Json.parse("[1,2,3]");
-        List<Json> array = json.asArray();
-        Collections.reverse(array);
+        Collections.reverse(json.asArray());
         assertEquals("[3,2,1]", json.toString());
     }
 
     @Test public void printModifiedObject() {
         Json json = Json.parse("{\"foo\":23}");
-        Map<String, Json> object = json.asObject();
-        object.put("bar", Json.value(42));
+        json.asObject().put("bar", Json.value(42));
         assertEquals("{\"foo\":23,\"bar\":42}", json.toString());
     }
 
